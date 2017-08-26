f9ccffd35271c557e8979cc1410ab0b7db62e708
staronline1985 <staronline1985@gmail.com>
2017-08-26 09:44:01 +0530

    Rest api changes

---
+++ Web.API/Web.API/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Http;
+
+
+namespace Web.API
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            var _formatter = GlobalConfiguration.Configuration.Formatters;
+            _formatter.Remove(_formatter.XmlFormatter);
+            System.Web.Routing.RouteTable.Routes.MapHttpRoute(
+               name: "DefaultApi",
+               routeTemplate: "api/{controller}/{id}",
+               defaults: new { id = System.Web.Http.RouteParameter.Optional });
+        }
+    }
+}
